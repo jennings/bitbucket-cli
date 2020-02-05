d695e899f77fa209639631ecf5e048aa54d56009
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-02-05 15:49:47 -0800

    add pr command skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
 import * as process from "process";
 import commander from "commander";
+import inquirer from "inquirer";
 
 commander
-  .command("hello [name]")
-  .description("display a friendly greeting")
-  .action((name?: string) => {
-    console.log(`Hello, ${name || "world"}!`);
+  .command("checkout [pr]")
+  .description("checkout a remote branch")
+  .action(async (pr: string, cmd: unknown) => {
+    if (pr != null) {
+      console.log(pr, typeof pr);
+    } else {
+      await inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "pr",
+            message: "Which pull request?",
+            choices: [
+              {
+                value: 123,
+                name: "Foo"
+              },
+              {
+                value: 456,
+                name: "Bar"
+              }
+            ]
+          }
+        ])
+        .then((answers: { pr: number }) => {
+          console.log("pr", answers.pr);
+        });
+    }
   });
 
-commander.parse(process.argv);
+commander.parseAsync(process.argv);
