c0b60fa3cc8238b73ff9b946caf9a9699e3fdec1
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-02-11 14:58:53 -0800

    remove unused function parameters

--- src/commands/pr.ts
+++ src/commands/pr.ts
@@ -9,63 +9,60 @@ export function createPrCommand(program: Command) {
     .command("pr:checkout [pr_number]")
     .alias("pr")
     .description("checkout a pull request branch")
-    .action(
-      async (pr_number: string | null, branch: string | null, cmd: Command) => {
-        const repo = await getCurrentRepo();
-        if (repo == null) {
-          throw Error("Not in a git repository");
-        }
-
-        if (pr_number != null) {
-          const response = await bitbucket.pullrequests.get({
-            ...repo,
-            pull_request_id: parseInt(pr_number)
-          });
-          checkoutPullRequest(response.data);
-        } else {
-          const response = await bitbucket.pullrequests.list({
-            ...repo,
-            state: "OPEN"
-          });
-
-          const openPullRequests = response.data.values!;
-
-          await inquirer
-            .prompt([
-              {
-                type: "list",
-                name: "pr_id",
-                message: "Which pull request?",
-                choices: openPullRequests.map(pr => ({
-                  name: pr.title,
-                  value: pr.id
-                }))
-              }
-            ])
-            .then(({ pr_id }: any) => {
-              const pullRequest = openPullRequests.find(p => p.id === pr_id);
-              if (pullRequest == null) {
-                throw Error(`Could not find pull request: ${pr_id}`);
-              }
-
-              checkoutPullRequest(pullRequest);
-            });
-        }
-
-        function checkoutPullRequest(pullRequest: Schema.Pullrequest): void {
-          const branch = pullRequest.source && pullRequest.source.branch;
-          if (branch == null || branch.name == null) {
-            throw Error(
-              `Pull request does not have a branchName: ${branch &&
-                branch.name}`
-            );
-          }
-
-          spawn("git", ["checkout", branch.name], {
-            shell: true,
-            stdio: "inherit"
-          });
-        }
+    .action(async (pr_number: string | null) => {
+      const repo = await getCurrentRepo();
+      if (repo == null) {
+        throw Error("Not in a git repository");
       }
-    );
+
+      if (pr_number != null) {
+        const response = await bitbucket.pullrequests.get({
+          ...repo,
+          pull_request_id: parseInt(pr_number)
+        });
+        checkoutPullRequest(response.data);
+      } else {
+        const response = await bitbucket.pullrequests.list({
+          ...repo,
+          state: "OPEN"
+        });
+
+        const openPullRequests = response.data.values!;
+
+        await inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "pr_id",
+              message: "Which pull request?",
+              choices: openPullRequests.map(pr => ({
+                name: pr.title,
+                value: pr.id
+              }))
+            }
+          ])
+          .then(({ pr_id }: any) => {
+            const pullRequest = openPullRequests.find(p => p.id === pr_id);
+            if (pullRequest == null) {
+              throw Error(`Could not find pull request: ${pr_id}`);
+            }
+
+            checkoutPullRequest(pullRequest);
+          });
+      }
+
+      function checkoutPullRequest(pullRequest: Schema.Pullrequest): void {
+        const branch = pullRequest.source && pullRequest.source.branch;
+        if (branch == null || branch.name == null) {
+          throw Error(
+            `Pull request does not have a branchName: ${branch && branch.name}`
+          );
+        }
+
+        spawn("git", ["checkout", branch.name], {
+          shell: true,
+          stdio: "inherit"
+        });
+      }
+    });
 }
